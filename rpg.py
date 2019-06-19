23d549506465e8ee505a8ae2c18810b1b5732628
Kartik Choudhary <kartik.c918@gmail.com>
2019-06-20 01:31:22 +0530

    Update rpg.py

--- rpg.py
+++ rpg.py
@@ -5,33 +5,19 @@ Created on Thu Jun 6 11:42:48 2019
 @author: Kartik
 """
 
-
 from consolemenu import *
 from consolemenu.items import *
 
-# Create the menu
+# Creating the menu
 menu = ConsoleMenu("CAVE PITCHBLACK", "A DnD style roll based RPG")
 
-# Create some items
-
-# MenuItem is the base class for all items, it doesn't do anything when selected
-menu_item = MenuItem("Menu Item")
-
 # A FunctionItem runs a Python function when selected
-function_item = FunctionItem("Call a Python function", func.intro(), ["Starting Game..."])
+start_game = FunctionItem("Start Game", func.intro(), ["Starting Game..."])
 
 # A CommandItem runs a console command
-command_item = CommandItem("Run a console command",  "touch hello.txt")
+leaderboard = MenuItem("View Leaderboards")
 
-# A SelectionMenu constructs a menu from a list of strings
-selection_menu = SelectionMenu(["item1", "item2", "item3"])
-
-# A SubmenuItem lets you add a menu (the selection_menu above, for example)
-# as a submenu of another menu
-submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
-
-# Once we're done creating them, we just add the items to the menu
-menu.append_item(menu_item)
+# Adding the items to the menu
 menu.append_item(function_item)
 menu.append_item(command_item)
 menu.append_item(submenu_item)
