e6fe30a513827198cfa7c1537d1f99781794ef10
shubham <shubhamgoel2525@gmail.com>
2019-08-10 01:38:11 +0530

    minor changes to rpg file

--- rpg.py
+++ rpg.py
@@ -5,14 +5,20 @@ Created on Thu Jun 6 11:42:48 2019
 @author: Kartik
 """
 #importing libraries
+import os as os
+
+os.system('cls')                 #clears screen before displaying ASCII art
+
+
 import objects as obj
 import functions as func
+
+os.system('cls')                 #clears screen again before initiating battle sequence
 
 
 playAgain = "Y"                  #to run the first instance
 
 
-        
 while playAgain == 'Y' or playAgain == 'y':
     func.intro()
     func.instance.score = 0
